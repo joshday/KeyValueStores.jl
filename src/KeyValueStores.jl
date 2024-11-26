37e4ba25e9db901618d24ed849733b7289a2bbfe
Josh Day <emailjoshday@gmail.com>
2024-11-26 14:29:59 -0500

    First round of testing

--- src/KeyValueStores.jl
+++ src/KeyValueStores.jl
@@ -1,5 +1,75 @@
 module KeyValueStores
 
-# Write your package code here.
+export Store
+
+#-----------------------------------------------------------------------------# AbstractKeyValueStore
+abstract type AbstractKeyValueStore{T} <: AbstractDict{Symbol, T} end
+
+dict(o::T) where {T <: AbstractKeyValueStore} = getfield(o, first(fieldnames(T)))
+
+Base.length(o::AbstractKeyValueStore) = length(dict(o))
+Base.keys(o::AbstractKeyValueStore) = keys(dict(o))
+Base.values(o::AbstractKeyValueStore) = values(dict(o))
+Base.iterate(o::AbstractKeyValueStore, state...) = iterate(dict(o), state...)
+
+const KeyLike = Union{AbstractString, Symbol}
+
+Base.getindex(o::AbstractKeyValueStore, k::KeyLike) = getindex(dict(o), Symbol(k))
+Base.setindex!(o::AbstractKeyValueStore, v, k::KeyLike) = setindex!(dict(o), v, Symbol(k))
+Base.get(o::AbstractKeyValueStore, k::KeyLike, default) = get(dict(o), Symbol(k), default)
+Base.get!(o::AbstractKeyValueStore, k::KeyLike, default) = get!(dict(o), Symbol(k), default)
+Base.haskey(o::AbstractKeyValueStore, k::AbstractString) = haskey(o, Symbol(k))
+
+Base.delete!(o::AbstractKeyValueStore, k::KeyLike) = delete!(dict(o), Symbol(k))
+
+Base.empty(o::T) where {T <: AbstractKeyValueStore} = T(empty(dict(o)))
+Base.empty!(o::AbstractKeyValueStore) = (empty!(dict(o)); o)
+
+Base.copy(o::T) where {T <: AbstractKeyValueStore} = T(copy(dict(o)))
+
+Base.sort(o::T; kw...) where {T <: AbstractKeyValueStore} = T(sort(dict(o); kw...))
+Base.sort!(o::AbstractKeyValueStore; kw...) = (sort!(dict(o); kw...); o)
+
+Base.merge(o::T, x::AbstractDict...) where {T <: AbstractKeyValueStore} = T(merge(dict(o), x...))
+Base.merge!(o::AbstractKeyValueStore, x::AbstractDict...) = (merge!(dict(o), x...); o)
+
+Base.propertynames(o::AbstractKeyValueStore) = keys(dict(o))
+Base.getproperty(o::AbstractKeyValueStore, k::Symbol) = getindex(o, k)
+Base.setproperty!(o::AbstractKeyValueStore, k::Symbol, v) = setindex!(o, v, k)
+
+# constructor helpers
+dict_arg() = ()
+dict_arg(x) = isempty(x) ? Pair{Symbol,Any}[] : (dict_arg(x) for x in x)
+dict_arg(x::Pair) = Symbol(x[1]) => x[2]
+
+
+#-----------------------------------------------------------------------------# Store
+struct Store{T, D <: AbstractDict{Symbol, T}} <: AbstractKeyValueStore{T}
+    dict::D
+    Store(dict::D) where {T, D <: AbstractDict{Symbol, T}} = new{T, D}(dict)
+end
+Store(x...; kw...) = Store(Dict(dict_arg(x)..., dict_arg(kw)...))
+Store{T}(x...; kw...) where {T} = Store(Dict{Symbol, T}(dict_arg(x)..., dict_arg(kw)...))
+Store{T, D}(x...; kw...) where {T, D <: AbstractDict{Symbol, T}} = Store(D(dict_arg(x)..., dict_arg(kw)...))
+
+#-----------------------------------------------------------------------------# prune!
+
+"""
+    prune!(x::AbstractDict)
+
+`delete!` all empty sub-dicts from `x`.
+"""
+prune!(x) = x
+
+function prune!(o::AbstractDict)
+    for (k, v) in pairs(o)
+        isempty(v) ? delete!(o, k) : prune!(v)
+    end
+end
+
+function delete_empty!(x)
+    Base.depwarn("delete_empty! is deprecated, use prune! instead", :delete_empty!; force=true)
+    prune!(x)
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,49 @@ using KeyValueStores
 using Test
 
 @testset "KeyValueStores.jl" begin
-    # Write your tests here.
+    @testset "Store" begin
+        @testset "Constructors" begin
+            # empty
+            @test Store() isa Store{Any, Dict{Symbol, Any}}
+            @test Store{Int}() isa Store{Int, Dict{Symbol, Int}}
+            @test Store{Int, Dict{Symbol, Int}}() isa Store{Int, Dict{Symbol, Int}}
+
+            # keyword
+            @test Store(x=1) isa Store{Int}
+            @test Store{Int}(x=1) isa Store{Int}
+            @test Store{Int, Dict{Symbol, Int}}(x=1) isa Store{Int, Dict{Symbol, Int}}
+
+            # pair
+            @test Store(:x => 1) isa Store{Int}
+            @test Store{Int}(:x => 1) isa Store{Int}
+            @test Store{Int, Dict{Symbol, Int}}(:x => 1) isa Store{Int, Dict{Symbol, Int}}
+
+            # pairs
+            @test Store(:x => 1, :y => 2) isa Store{Int}
+            @test Store{Int}(:x => 1, :y => 2) isa Store{Int}
+            @test Store{Int, Dict{Symbol, Int}}(:x => 1, :y => 2) isa Store{Int}
+
+            # dict
+            @test Store(Dict(:x => 1)) isa Store{Int}
+            @test Store{Int}(Dict(:x => 1)) isa Store{Int}
+            @test Store{Int, Dict{Symbol, Int}}(Dict(:x => 1)) isa Store{Int, Dict{Symbol, Int}}
+        end
+        @testset "Base functions" begin
+            o = Store(x=1, y=2)
+            @test sort!(collect(keys(o))) == [:x, :y]
+            @test sort!(collect(propertynames(o))) == [:x, :y]
+            @test get(o, :z, 3) == 3
+            @test !haskey(o, :z)
+            @test get!(o, :z, 3) == 3
+            @test haskey(o, :z)
+            delete!(o, :z)
+            @test !haskey(o, :z)
+            @test o.x == o[:x] == o["x"] == 1
+            @test o.y == o[:y] == o["y"] == 2
+            o.z = 3
+            o[:z] = 3
+            o["z"] = 3
+            @test o.z == o[:z] == o["z"] == 3
+        end
+    end
 end
