5574f1ffd4a5b31e0cad3a401080d58691bad0e1
Josh Day <emailjoshday@gmail.com>
2024-11-28 14:07:31 -0500

    wip

--- src/KeyValueStores.jl
+++ src/KeyValueStores.jl
@@ -1,75 +1,97 @@
 module KeyValueStores
 
-export Store
+using OrderedCollections
 
-#-----------------------------------------------------------------------------# AbstractKeyValueStore
-abstract type AbstractKeyValueStore{T} <: AbstractDict{Symbol, T} end
+export Store, DefaultStore, Default, NestedStore
 
-dict(o::T) where {T <: AbstractKeyValueStore} = getfield(o, first(fieldnames(T)))
+#-----------------------------------------------------------------------------# AbstractStore
+abstract type AbstractStore{T} <: AbstractDict{Symbol, T} end
 
-Base.length(o::AbstractKeyValueStore) = length(dict(o))
-Base.keys(o::AbstractKeyValueStore) = keys(dict(o))
-Base.values(o::AbstractKeyValueStore) = values(dict(o))
-Base.iterate(o::AbstractKeyValueStore, state...) = iterate(dict(o), state...)
+# function Base.summary(io::IO, o::AbstractStore{T}) where {T}
+#     print(io, typeof(o))
+# end
 
-const KeyLike = Union{AbstractString, Symbol}
+dict(o::T) where {T <: AbstractStore} = getfield(o, first(fieldnames(T)))
 
-Base.getindex(o::AbstractKeyValueStore, k::KeyLike) = getindex(dict(o), Symbol(k))
-Base.setindex!(o::AbstractKeyValueStore, v, k::KeyLike) = setindex!(dict(o), v, Symbol(k))
-Base.get(o::AbstractKeyValueStore, k::KeyLike, default) = get(dict(o), Symbol(k), default)
-Base.get!(o::AbstractKeyValueStore, k::KeyLike, default) = get!(dict(o), Symbol(k), default)
-Base.haskey(o::AbstractKeyValueStore, k::AbstractString) = haskey(o, Symbol(k))
+Base.copy(o::T) where {T <: AbstractStore} = T(copy(dict(o)))
+Base.iterate(o::AbstractStore, state...) = iterate(dict(o), state...)
 
-Base.delete!(o::AbstractKeyValueStore, k::KeyLike) = delete!(dict(o), Symbol(k))
+# pass-to-dict methods
+for f in (:length, :keys, :values)
+    @eval Base.$f(o::AbstractStore) = $f(dict(o))
+end
 
-Base.empty(o::T) where {T <: AbstractKeyValueStore} = T(empty(dict(o)))
-Base.empty!(o::AbstractKeyValueStore) = (empty!(dict(o)); o)
+# Second-arg-is-a-key methods
+for f in (:getindex, :haskey, :delete!)
+    @eval Base.$f(o::AbstractStore, k::Symbol) = $f(dict(o), k)
+    @eval Base.$f(o::AbstractStore, k::AbstractString) = $f(o, Symbol(k))
+end
+Base.setindex!(o::AbstractStore, v, k::Symbol) = setindex!(dict(o), v, k)
+Base.setindex!(o::AbstractStore, v, k::AbstractString) = setindex!(o, v, Symbol(k))
 
-Base.copy(o::T) where {T <: AbstractKeyValueStore} = T(copy(dict(o)))
+# get/get!
+for f in [:get, :get!]
+    @eval Base.$f(o::AbstractStore, k::Symbol, default) = $f(dict(o), k, default)
+    @eval Base.$f(o::AbstractStore, k::AbstractString, default) = $f(o, Symbol(k), default)
+end
 
-Base.sort(o::T; kw...) where {T <: AbstractKeyValueStore} = T(sort(dict(o); kw...))
-Base.sort!(o::AbstractKeyValueStore; kw...) = (sort!(dict(o); kw...); o)
+# mutating and copy-based methods
+Base.empty(o::AbstractStore) = empty!(copy(o))
+Base.empty!(o::AbstractStore) = (empty!(dict(o)); o)
+Base.sort(o::AbstractStore; kw...) = sort!(copy(o); kw...)
+Base.sort!(o::AbstractStore; kw...) = (sort!(dict(o); kw...); o)
+Base.merge(o::AbstractStore, x::AbstractDict...) = merge!(copy(o), x...)
+Base.merge!(o::AbstractStore, x::AbstractDict...) = (merge!(dict(o), x...); o)
 
-Base.merge(o::T, x::AbstractDict...) where {T <: AbstractKeyValueStore} = T(merge(dict(o), x...))
-Base.merge!(o::AbstractKeyValueStore, x::AbstractDict...) = (merge!(dict(o), x...); o)
-
-Base.propertynames(o::AbstractKeyValueStore) = keys(dict(o))
-Base.getproperty(o::AbstractKeyValueStore, k::Symbol) = getindex(o, k)
-Base.setproperty!(o::AbstractKeyValueStore, k::Symbol, v) = setindex!(o, v, k)
+# get/set property
+Base.propertynames(o::AbstractStore) = keys(dict(o))
+Base.getproperty(o::AbstractStore, k::Symbol) = getindex(o, k)
+Base.setproperty!(o::AbstractStore, k::Symbol, v) = setindex!(o, v, k)
 
 # constructor helpers
 dict_arg() = ()
 dict_arg(x) = isempty(x) ? Pair{Symbol,Any}[] : (dict_arg(x) for x in x)
 dict_arg(x::Pair) = Symbol(x[1]) => x[2]
 
-
 #-----------------------------------------------------------------------------# Store
-struct Store{T, D <: AbstractDict{Symbol, T}} <: AbstractKeyValueStore{T}
+struct Store{T, D <: AbstractDict{Symbol, T}} <: AbstractStore{T}
     dict::D
     Store(dict::D) where {T, D <: AbstractDict{Symbol, T}} = new{T, D}(dict)
 end
-Store(x...; kw...) = Store(Dict(dict_arg(x)..., dict_arg(kw)...))
-Store{T}(x...; kw...) where {T} = Store(Dict{Symbol, T}(dict_arg(x)..., dict_arg(kw)...))
+Store(x...; kw...) = Store(OrderedDict(dict_arg(x)..., dict_arg(kw)...))
+Store{T}(x...; kw...) where {T} = Store(OrderedDict{Symbol, T}(dict_arg(x)..., dict_arg(kw)...))
 Store{T, D}(x...; kw...) where {T, D <: AbstractDict{Symbol, T}} = Store(D(dict_arg(x)..., dict_arg(kw)...))
 
-#-----------------------------------------------------------------------------# prune!
 
-"""
-    prune!(x::AbstractDict)
-
-`delete!` all empty sub-dicts from `x`.
-"""
-prune!(x) = x
-
-function prune!(o::AbstractDict)
-    for (k, v) in pairs(o)
-        isempty(v) ? delete!(o, k) : prune!(v)
-    end
+#-----------------------------------------------------------------------------# DefaultStore
+struct DefaultStore{T, D, S <: Store{T, D}, R} <: AbstractStore{Union{T, R}}
+    store::S
+    default::R
+    DefaultStore(store::S, default::R) where {T, D, S <: Store{T, D}, R} = new{T, D, S, R}(store, default)
 end
+DefaultStore(default, x...; kw...) = DefaultStore(Store(x...; kw...), default)
+DefaultStore{T}(default, x...; kw...) where {T} = DefaultStore(Store{T}(x...; kw...), default)
+DefaultStore{T, D}(default, x...; kw...) where {T, D} = DefaultStore(Store{T, D}(x...; kw...), default)
 
-function delete_empty!(x)
-    Base.depwarn("delete_empty! is deprecated, use prune! instead", :delete_empty!; force=true)
-    prune!(x)
+Base.getindex(o::DefaultStore, k::Symbol) = haskey(o, k) ? dict(o)[k] : getfield(o, :default)
+
+
+#-----------------------------------------------------------------------------# NestedStore
+struct NestedStore{D, S <: Store{Any, D}} <: AbstractStore{Any}
+    store::S
+    path::Vector{Symbol}
+end
+NestedStore(x...; kw...) = NestedStore(Store{Any}(x...; kw...), Symbol[])
+NestedStore{D}(x...; kw...) where {D} = NestedStore(Store{Any, D}(x...; kw...), Symbol[])
+
+Base.getindex(o::NestedStore, k::Symbol) = haskey(o, k) ? dict(o)[k] : NestedStore(getfield(o, :store), [getfield(o, :path); k])
+
+function Base.setindex!(o::NestedStore{D, S}, v, k::Symbol) where {D, S}
+    obj = dict(o)
+    for p in getfield(o, :path)
+        obj = get!(obj, p, S())
+    end
+    obj[k] = v
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,13 @@
 using KeyValueStores
 using Test
+using OrderedCollections
 
 @testset "KeyValueStores.jl" begin
     @testset "Store" begin
         @testset "Constructors" begin
             # empty
-            @test Store() isa Store{Any, Dict{Symbol, Any}}
-            @test Store{Int}() isa Store{Int, Dict{Symbol, Int}}
+            @test Store() isa Store{Any, OrderedDict{Symbol, Any}}
+            @test Store{Int}() isa Store{Int, OrderedDict{Symbol, Int}}
             @test Store{Int, Dict{Symbol, Int}}() isa Store{Int, Dict{Symbol, Int}}
 
             # keyword
@@ -45,6 +46,20 @@ using Test
             o[:z] = 3
             o["z"] = 3
             @test o.z == o[:z] == o["z"] == 3
+        end
+        @testset "DefaultStore" begin
+            d = DefaultStore(nothing, x=1, y=2)
+            @test d.x == d["x"] == d[:x] == 1
+            @test d.z === d["z"] === d[:z] === nothing
+            @test length(d) == 2
+            @test get(d, :z, 3) == 3
+        end
+        @testset "NestedStore" begin
+            o = NestedStore(x=1, y=2)
+            @test o.x == o[:x] == o["x"] == 1
+            @test (o.a.b.c.d = 5) == 5
+            @test o.a.b.c.d == 5
+            @test o.a isa Store
         end
     end
 end
